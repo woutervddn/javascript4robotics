90c1516a140fd3500ef51fce396e8c6e9e07b726
Wouter Vandenneucker <wouter@vandenneucker.be>
2015-10-01 12:36:10 +0200

    added servo_go_to_pos function to the dsscx18s library

--- src/app.js
+++ src/app.js
@@ -17,12 +17,24 @@ Cylon.robot({
 
     //every((1).second(), console.log("test"));
 
-    every((.5).second(), function(){
-        my.dsscx18s.testSer1(function(err, version) {return 0;});
-        after((.25).seconds(), function() {
-          my.dsscx18s.testSer1_b(function(err, version) {return 0;});
-        });
-      }
-    );
+    // every((3).second(), function(){
+    //     my.dsscx18s.testSer1(function(err, version) {return 0;});
+    //     after((1.5).seconds(), function() {
+    //       my.dsscx18s.testSer1_b(function(err, version) {return 0;});
+    //     });
+    //   }
+    // );
+    var servoPin = 1;
+    var newPosition = 200;
+    var actuate = true;
+    var callback = function(){
+      return 0;
+    }
+    my.dsscx18s.servo_go_to_pos(servoPin, newPosition, actuate, callback);
+
+    after((1.5).seconds(), function() {
+      newPosition = 30;
+      my.dsscx18s.servo_go_to_pos(servoPin, newPosition, actuate, callback);
+    });
   }
 }).start();
