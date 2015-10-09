cdfdae5207f7136da0259e71347ce62439939f22
Wouter Vandenneucker <wouter@vandenneucker.be>
2015-10-09 09:12:15 +0200

    changed install script to also do the npm install

--- cylonjs/tools/ubuntu_install.sh
+++ cylonjs/tools/ubuntu_install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-apt-get install nodejs npm build-essential nodejs-legacy
+## Install dependencies
+#apt-get install nodejs npm build-essential nodejs-legacy
+
+## Install application
+DIR="$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ))"
+
+## Copy source files to bin folder
+cp -r "$DIR/src" "$DIR/bin/"
+
+##npm install
+cd "$DIR/bin/"
+npm install
